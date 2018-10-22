7480f3bd1b029cfb36ca1013d603af5e70ddbe76
w190510995 <ws2017@aliyun.com>
2018-10-22 11:48:28 +0800

    2

--- pointInfo/views_classic_2.py
+++ pointInfo/views_classic_2.py
@@ -2,3 +2,10 @@
 #Version: V 1.0
 author: 'WangSheng'
 date: '2018/10/22 11:45'
+
+from django.db.models import Q
+from django.http import JsonResponse,HttpResponseRedirect
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
