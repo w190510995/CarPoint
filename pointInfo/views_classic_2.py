4bf6ac9bac441d9c5379950ee6c1752735532bfd
w190510995 <ws2017@aliyun.com>
2018-10-22 11:55:40 +0800

    2

--- pointInfo/views_classic_2.py
+++ pointInfo/views_classic_2.py
@@ -9,3 +9,4 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth
+from .models import *
